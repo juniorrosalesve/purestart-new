2ed159082fab5853efc92f75a96066ccca214ba4
Júnior Rosales <74562615+juniorrosalesve@users.noreply.github.com>
2023-11-27 11:20:11 -0500

    fixes

--- database/seeders/SeederPosts.php
+++ database/seeders/SeederPosts.php
@@ -17,22 +17,22 @@ class SeederPosts extends Seeder
         $posts  =   [
             [
                 'Revolutionizing Cleaning: Our Journey',
-                'Join us as we take you through our journey of creating a cleaner, healthier world...',
+                'Join us as we take you through our journey of creating a cleaner, healthier world. We started with a simple idea - to make cleaning services more accessible and effective. Over the years, we’ve developed innovative cleaning solutions and methods that not only clean but also enhance the health and wellbeing of our customers. In this post, we share our story, our challenges, and how we overcame them to revolutionize the cleaning industry.',
                 '01.jpeg'
             ],
             [
-                'Revolutionizing Cleaning: Our Journey',
-                'Discover our top 5 cleaning tips to keep your home sparkling clean...',
+                'Top 5 Cleaning Tips for Your Home',
+                'Discover our top 5 cleaning tips to keep your home sparkling clean. A clean home is a healthy home, and with our expert cleaning tips, you can ensure your home is not only clean but also a safe and comfortable space for you and your family. From the best cleaning products to use, to tips on maintaining cleanliness, this post covers it all.',
                 '02.jpg'
             ],
             [
-                'Revolutionizing Cleaning: Our Journey',
-                'Learn about the science that powers our innovative cleaning solutions...',
+                'The Science Behind Our Cleaning Solutions',
+                'Learn about the science that powers our innovative cleaning solutions. At our company, we believe in the power of science to create solutions that are not only effective but also safe and environmentally friendly. In this post, we delve into the scientific principles that guide our product development and how these result in superior cleaning solutions.',
                 '03.jpg'
             ],
             [
-                'Revolutionizing Cleaning: Our Journey',
-                'Get to know the dedicated team members who are passionate about cleaning...',
+                'Meet Our Team: The People Behind the Clean',
+                'Get to know the dedicated team members who are passionate about cleaning. Our team is our strength, and in this post, we introduce you to the people who work tirelessly to ensure you have a clean and healthy living space. From our cleaning experts to our customer service representatives, meet the people who make it all happen.',
                 '04.jpg'
             ]
         ];
--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -7,7 +7,7 @@
             <div class="bg-white rounded-lg shadow-md p-6">
                 <h2 class="text-primary-light text-2xl mb-2 abril">{{ $post->nombre }}</h2>
                 <p class="text-sm text-gray-600">{{ $post->created_at->format('F j, Y') }}</p>
-                <img class="my-4 rounded" src="{{ asset('storage/posts/'.$post->image) }}" alt="Image of blog post 1">
+                <img class="my-4 rounded mx-auto" src="{{ asset('storage/posts/'.$post->image) }}" alt="Image of blog post 1">
                 <p class="my-4 lato">{{ $post->descripcion }}</p>
             </div>
         </div>
